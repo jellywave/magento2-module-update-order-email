1e5e8c8c6531278b33a45f807ac53dc5345769f3
Chris Diaper <chris@jellywave.com>
2020-09-19 20:47:05 +0100

    Init Module

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar as ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Jellywave_UpdateOrderEmail',
+    __DIR__
+);
